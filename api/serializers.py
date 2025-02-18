63b971d3ebb80957a8de0107e9139986844fc010
johnchleung <johnchleung@nec.com>
2025-02-18 16:34:55 +0800

    Update Serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import Tbltib
+from .models import Tib
 
 
-class TbltibSerializer(serializers.ModelSerializer):
+class TibSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tbltib
-        fields = ('fldID', 'fldNo', 'fldStatus', 'fldCoordinate ',
-                    'fldOrientation', 'fldPicture ', 'fldLastMDate', 
-                    'fldEmpNameM ')
+        model = Tib
+        fields = ('fldID', 'fldNo', 'fldStatus', 'fldCoordinate',
+                    'fldOrientation', 'fldPicture', 'fldLastMDate', 
+                    'fldEmpNameM')
         
         # fields = '__all__'
