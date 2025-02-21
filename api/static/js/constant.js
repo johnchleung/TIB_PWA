67b890a706489947f5c3f141fbb0c448bec593ab
johnchleung <johnchleung@nec.com>
2025-02-21 10:58:29 +0800

    Add frontend elements for UI

---
+++ api/static/js/constant.js
@@ -0,0 +1,40 @@
+function Constant(){
+
+	// Labels & messages
+	LBL_YOU_ARE_HERE  = "You are here";
+	LBL_TFR_DESK      = "Transfer Desk";
+	LBL_HOW_TO_ACCESS = "How to access";
+	LBL_EST_TIME      = "Estimated time";
+	LBL_ENQ_TD_ROUTE  = "Enquiry about the route of transfer desk";
+	MSG_TRANSFER      = "Transfer";
+	I_TD_INFO       = "Transfer Desks Information";
+
+	// Airline Info
+	I_TRF_DESK           = "Transfer desk";
+	I_CURR_OPER_HOUR     = "Current operating hours";
+	I_ENQ_HOTLINE        = "Enquiry Hotline";
+    ERR_NO_AIRLINE_FOUND = "Cannot find the relevent airline";
+
+	// Transfer type
+	TYPE_WALK_EN = "Walking";
+	TYPE_APM_EN  = "Automated People Mover";
+	I_W_OBP      = "WITH onward boarding pass";
+	I_WO_OBP     = "WITHOUT onward boarding pass";
+	
+    I_FERRY        = "Ferries to Mainland / Macau";
+	I_FERRY_INFO   = "Mainland / Macau Ferries";
+	I_M_FERRY_DESC = "Proceed to \" " + I_FERRY_INFO + " \" for ticketing and ferry boarding";
+	I_M_FERRY_NOTE = "Immigration clearance and baggage reclaim and NOT required";
+
+	// Measurement unit
+	UNIT_METER = "M"; // Meters
+	UNIT_MINS  = "Mins"; // Time in minutes
+	UNIT_MIN  = "Min"; // Time in 1 minute
+	
+	// Button labels
+	BTN_SUGGEST_ROUTE = "Suggested route";
+	BTN_BACK_TO_PREV  = "Back to previous";
+	BTN_TRF_PROCEDURE = "Transfer procedures";
+	BTN_CLICK_HERE    = "Please click here";
+	
+}
